69da5f9e0854c528b8be2eb621ba3d83b5246989
alastirm <bruno.vermont@gmail.com>
2017-11-17 19:33:18 +0100

    change décroissance CINT

--- model/src/main/java/com/ed/cgdd/derby/finance/impl/TypeFinanceServiceImpl.java
+++ model/src/main/java/com/ed/cgdd/derby/finance/impl/TypeFinanceServiceImpl.java
@@ -95,8 +95,20 @@ public abstract class TypeFinanceServiceImpl implements TypeFinanceService {
 //		long startCINT = System.currentTimeMillis();
 
 		// XXX integration CINT
-		BigDecimal coutIntSys = coutIntangibleSys.multiply(
-				getVariation(geste.getSysChaud(), annee, evolCoutTechno), MathContext.DECIMAL32);
+		//BigDecimal coutIntSys = coutIntangibleSys.multiply(
+		//		getVariation(geste.getSysChaud(), annee, evolCoutTechno), MathContext.DECIMAL32);
+		// modif barbare test
+		BigDecimal coutIntSys = coutIntangibleSys;
+		if(coutIntangibleSys.compareTo(BigDecimal.ZERO) <0 ){
+		coutIntSys = coutIntangibleSys;
+			
+		} else {
+			double variation = (2009-annee) * (0.05) + 1 ; 
+		coutIntSys = coutIntangibleSys.multiply(new BigDecimal(variation, MathContext.DECIMAL32));
+		}	
+		
+		
+		
 		BigDecimal multi = BigDecimal.ONE;
 		if (geste.getTypeRenovBati() != TypeRenovBati.ETAT_INIT) {
 			multi = getVariation(geste.getTypeRenovBati().getLabel(), annee, evolCoutBati);
