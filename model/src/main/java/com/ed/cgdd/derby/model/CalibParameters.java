c30b413371e7e0db253d11a22e17f42a0532d74e
alastirm <bruno.vermont@gmail.com>
2019-02-21 13:05:50 +0100

    Commit mars 2019 version THEMA

--- model/src/main/java/com/ed/cgdd/derby/model/CalibParameters.java
+++ model/src/main/java/com/ed/cgdd/derby/model/CalibParameters.java
@@ -4,17 +4,23 @@ import java.math.BigDecimal;
 
 public class CalibParameters {
 	
-	// annee de calibration (prix des energies)
+	// annee de calibration (prix des energies), choix du prix des energies pour la calibration (NE PAS TOUCHER)
 	public static final Integer YEAR_CALIB=2009;
-	
+	// choix du taux d'actualisation pour la calibration (NE PAS TOUCHER)
 	public static final BigDecimal TAUX_ACTU_CALIB = new BigDecimal("0.04");
 	
-	// param surcout fuel switch en % du cout d'investissement
+	// Ci dessous un ensemble de parametres sur les couts d'investissements qui permettent de modifier la calibration 
+	// des parts de marche des systemes et le niveau d'investissement
+	// Utile pour faire des tests de sensibilite surtout sinon ne pas modifier
+	
+	// parametre surcout du fait du changement d'energie % du cout d'investissement
 		public final static boolean  checkSurcoutFuelSwitch = true;
+		// surcout si on passe d'un systeme centralise (gaz, fioul...) a decentralise (elec...)
 		public final static BigDecimal FacteurFuelCentrElec = new BigDecimal("0");
+		// surcout si on passe d'un decentralise (elec...) à un systeme centralise (gaz, fioul...)
 		public final static BigDecimal FacteurFuelElecCentr = new BigDecimal("0");
 	
-    // param surcout systemes electriques (facteur multiplicatif du cout d'investissement)
+    // parametre surcout systemes electriques (facteur multiplicatif du cout d'investissement)
 		public final static boolean  checkSurcoutSyst = false; 
 //		public final static BigDecimal FacteurCINVelecJoule = new BigDecimal("0.4");
 //		public final static BigDecimal FacteurCINVelecPAC = new BigDecimal("0.6");
@@ -26,28 +32,36 @@ public class CalibParameters {
 		public final static BigDecimal FacteurCINVGaz = new BigDecimal("1");
 		public final static BigDecimal FacteurCINVGazPerf = new BigDecimal("1");
 		
-	// param surcout ENSBBC (facteur multiplicatif des couts)
+	// parametre surcout ENSBBC (facteur multiplicatif des couts des gestes)
 		public final static boolean  checkSurcoutENSBBC = false;
 		public final static BigDecimal FacteurENSBBC = new BigDecimal("1");
+	// parametre surcout ENSMOD (facteur multiplicatif des couts des gestes)
 		public final static boolean  checkSurcoutENSMOD =false;
 		public final static BigDecimal FacteurENSMOD = new BigDecimal("1");
+	// parametre surcout pour tous les gestes sur le bati (facteur multiplicatif des couts des gestes)
 		public final static boolean  checkSurcoutall = false;
 		public final static BigDecimal Facteurall = new BigDecimal("1");
+
+	// CALIBRATION DU NIVEAU D'INVESTISSEMENT DANS LA RENOVATION DU BATI
+	// REMPLACE LES COUTS INTANGIBLES
+	// les parametres lambda correspondent a des facteurs multiplicatifs du cout global du geste NE RIEN FAIRE
+	// Plus on diminue le Lambda, plus il est interessant de ne rien faire cad ne pas investir
+	// on peut ainsi controler le niveau d'investissement sur les annees initiales
+	
 		
-	// lambda cout intangible ne rien faire (pourcentage de reduction des charges initiales)
+	// lambda cout intangible ne rien faire (pourcentage de reduction des charges initiales) 
+	// pour l'ensemble du parc
+	// NE PAS TOUCHER laisser a 1
 		public final static BigDecimal LambdaNRF = new BigDecimal("1");
 	
-//	// lambda par branche 2050
-//		public final static BigDecimal LambdaNRF01 = new BigDecimal("1");
-//		public final static BigDecimal LambdaNRF02 = new BigDecimal("1");
-//		public final static BigDecimal LambdaNRF03 = new BigDecimal("1");
-//		public final static BigDecimal LambdaNRF04 = new BigDecimal("1");
-//		public final static BigDecimal LambdaNRF05 = new BigDecimal("1");
-//		public final static BigDecimal LambdaNRF06 = new BigDecimal("1");
-//		public final static BigDecimal LambdaNRF07 = new BigDecimal("1");
-//		public final static BigDecimal LambdaNRF08 = new BigDecimal("1");
+	// ICI on parametre le lambda pour chaque branche du parc
+	// on donne une valeur initiale pour 2010 et une valeur finale pour 2050
+	// la valeur finale doit etre superieur a la valeur initiale
+	// le lambda croit ensuite lineairement entre la valeur initiale et la valeur finale
+	// mettre la meme valeur initiale et finale si on veut un lambda constant
 		
-//		
+	// VALEURS POUR LES SCENARIOS DGEC AMS-AME 2017	 (A GARDER AU CAS OU)
+		
 //////		// lambda par branche 2050 AMS-AME 2017
 //		public final static BigDecimal LambdaNRF01 = new BigDecimal("0.4");
 //		public final static BigDecimal LambdaNRF02 = new BigDecimal("0.7");
@@ -58,7 +72,7 @@ public class CalibParameters {
 //		public final static BigDecimal LambdaNRF07 = new BigDecimal("0.65");
 //		public final static BigDecimal LambdaNRF08 = new BigDecimal("0.65");
 ////				
-////		// lambda par branche debut de periode
+////		// lambda par branche debut de periode AMS-AME 2017
 //		public final static BigDecimal LambdaNRF01Debut  =  new BigDecimal("0.2");
 //		public final static BigDecimal LambdaNRF02Debut  = new BigDecimal("0.7");
 //		public final static BigDecimal LambdaNRF03Debut  = new BigDecimal("0.7");
@@ -68,30 +82,53 @@ public class CalibParameters {
 //		public final static BigDecimal LambdaNRF07Debut  = new BigDecimal("0.6");
 //		public final static BigDecimal LambdaNRF08Debut  = new BigDecimal("0.6");
 //		
-//		
-	//// lambda analyse de sensib	INV - 20 %	
-	// lambda par branche 2050 AMS-AME 2017
-	public final static BigDecimal LambdaNRF01 = new BigDecimal("0.32");
-	public final static BigDecimal LambdaNRF02 = new BigDecimal("0.56");
-	public final static BigDecimal LambdaNRF03 = new BigDecimal("0.56");
-	public final static BigDecimal LambdaNRF04 = new BigDecimal("0.56");
-	public final static BigDecimal LambdaNRF05 = new BigDecimal("0.64");
-	public final static BigDecimal LambdaNRF06 = new BigDecimal("0.4");
-	public final static BigDecimal LambdaNRF07 = new BigDecimal("0.52");
-	public final static BigDecimal LambdaNRF08 = new BigDecimal("0.52");
+		
+		// VALEURS POUR LA VERSION CALIBREE SUR LES ETUDES CODA STRATEGIES
+			
+////	// lambda par branche 2050 calibration sur CODA 
+	public final static BigDecimal LambdaNRF01 = new BigDecimal("0.4");
+	public final static BigDecimal LambdaNRF02 = new BigDecimal("0.85");
+	public final static BigDecimal LambdaNRF03 = new BigDecimal("0.7");
+	public final static BigDecimal LambdaNRF04 = new BigDecimal("0.7");
+	public final static BigDecimal LambdaNRF05 = new BigDecimal("0.8");
+	public final static BigDecimal LambdaNRF06 = new BigDecimal("0.5");
+	public final static BigDecimal LambdaNRF07 = new BigDecimal("0.85");
+	public final static BigDecimal LambdaNRF08 = new BigDecimal("0.65");
 //			
-//	// lambda par branche debut de periode
-	public final static BigDecimal LambdaNRF01Debut  =  new BigDecimal("0.2");
-	public final static BigDecimal LambdaNRF02Debut  = new BigDecimal("0.56");
-	public final static BigDecimal LambdaNRF03Debut  = new BigDecimal("0.56");
-	public final static BigDecimal LambdaNRF04Debut  = new BigDecimal("0.56");
-	public final static BigDecimal LambdaNRF05Debut  = new BigDecimal("0.64");
-	public final static BigDecimal LambdaNRF06Debut  = new BigDecimal("0.3");
-	public final static BigDecimal LambdaNRF07Debut  = new BigDecimal("0.52");
-	public final static BigDecimal LambdaNRF08Debut  = new BigDecimal("0.52");
+//	// lambda par branche debut de periode calibration sur CODA 
+	public final static BigDecimal LambdaNRF01Debut  =  new BigDecimal("0.33");
+	public final static BigDecimal LambdaNRF02Debut  = new BigDecimal("0.85");
+	public final static BigDecimal LambdaNRF03Debut  = new BigDecimal("0.68");
+	public final static BigDecimal LambdaNRF04Debut  = new BigDecimal("0.68");
+	public final static BigDecimal LambdaNRF05Debut  = new BigDecimal("0.58");
+	public final static BigDecimal LambdaNRF06Debut  = new BigDecimal("0.37");
+	public final static BigDecimal LambdaNRF07Debut  = new BigDecimal("0.85");
+	public final static BigDecimal LambdaNRF08Debut  = new BigDecimal("0.6");
+			
+		
+	// VALEURS POUR  analyse de sensibilité sur le niveau d'investissement
 
-		
-		
+//	// lambda par branche 2050 AMS-AME 2017
+//	public final static BigDecimal LambdaNRF01 = new BigDecimal("0.25");
+//	public final static BigDecimal LambdaNRF02 = new BigDecimal("0.60");
+//	public final static BigDecimal LambdaNRF03 = new BigDecimal("0.55");
+//	public final static BigDecimal LambdaNRF04 = new BigDecimal("0.55");
+//	public final static BigDecimal LambdaNRF05 = new BigDecimal("0.60");
+//	public final static BigDecimal LambdaNRF06 = new BigDecimal("0.35");
+//	public final static BigDecimal LambdaNRF07 = new BigDecimal("0.60");
+//	public final static BigDecimal LambdaNRF08 = new BigDecimal("0.40");
+////			
+////	// lambda par branche debut de periode
+//	public final static BigDecimal LambdaNRF01Debut  =  new BigDecimal("0.25");
+//	public final static BigDecimal LambdaNRF02Debut  = new BigDecimal("0.60");
+//	public final static BigDecimal LambdaNRF03Debut  = new BigDecimal("0.55");
+//	public final static BigDecimal LambdaNRF04Debut  = new BigDecimal("0.55");
+//	public final static BigDecimal LambdaNRF05Debut  = new BigDecimal("0.60");
+//	public final static BigDecimal LambdaNRF06Debut  = new BigDecimal("0.35");
+//	public final static BigDecimal LambdaNRF07Debut  = new BigDecimal("0.60");
+//	public final static BigDecimal LambdaNRF08Debut  = new BigDecimal("0.40");
+//
+//	
 //// lambda analyse de sensib
 //////	// lambda par branche 2050
 //	public final static BigDecimal LambdaNRF01 = new BigDecimal("0.2");
@@ -135,7 +172,9 @@ public class CalibParameters {
 //		public final static BigDecimal LambdaNRF08Debut  = new BigDecimal("0.48");
 //		
 		
-	// param recalage du parc par branche en fonction du CEREN 2010 
+	// parametres de recalage des surfaces du parc par branche en fonction du CEREN 2010 
+	// il s'agit ici de facteurs multiplicatifs des surfaces par branche en 2010 pour retrouver les surfaces du CEREN
+	// METTRE 1 pour enlever le recalage
 		public final static BigDecimal CalageBranche01 = new BigDecimal("1.0239862");
 		public final static BigDecimal CalageBranche02 = new BigDecimal("1.0420552");
 		public final static BigDecimal CalageBranche03 = new BigDecimal("1.0163286");
@@ -145,13 +184,9 @@ public class CalibParameters {
 		public final static BigDecimal CalageBranche07 = new BigDecimal("0.9779685");
 		public final static BigDecimal CalageBranche08 = new BigDecimal("0.9979434");
 		
-	// param recalage du parc par énergie de chauffage en fonction du CEREN 2010 
-//		public final static BigDecimal CalageParcChauffElec 	= new BigDecimal("1.0420273");
-//		public final static BigDecimal CalageParcChauffGaz 		= new BigDecimal("0.9924076");
-//		public final static BigDecimal CalageParcChauffFioul 	= new BigDecimal("0.9203859");
-//		public final static BigDecimal CalageParcChauffUrbain 	= new BigDecimal("1.1050800");
-//		public final static BigDecimal CalageParcChauffAutres 	= new BigDecimal("1.1050800");
-//		 
+	// parametres de recalage du parc par énergie de chauffage en fonction du CEREN 2010 
+	// il s'agit ici de facteurs multiplicatifs des surfaces par branche en 2010 pour retrouver les surfaces du CEREN
+	// METTRE 1 pour enlever le recalage
 		public final static BigDecimal CalageParcChauffElec 	= new BigDecimal("1.0419581");
 		public final static BigDecimal CalageParcChauffGaz 		= new BigDecimal("0.9996030");
 		public final static BigDecimal CalageParcChauffFioul 	= new BigDecimal("0.9226443");
@@ -163,29 +198,29 @@ public class CalibParameters {
 //		public final static BigDecimal CalageParcChauffFioul 	= new BigDecimal("1");
 //		public final static BigDecimal CalageParcChauffUrbain 	= new BigDecimal("1");
 //		public final static BigDecimal CalageParcChauffAutres 	= new BigDecimal("1");
-//				
-//	// param recalage du parc par énergie de chauffage en fonction du CEREN 2010 
-//		public final static BigDecimal CalageParcChauffElec 	= new BigDecimal("1.0649036");
-//		public final static BigDecimal CalageParcChauffGaz 		= new BigDecimal("0.9970268");
-//		public final static BigDecimal CalageParcChauffFioul 	= new BigDecimal("0.9065154");
-//		public final static BigDecimal CalageParcChauffUrbain 	= new BigDecimal("1.0547371");
-//		public final static BigDecimal CalageParcChauffAutres 	= new BigDecimal("1.0547371");
-//			
-////	// param recalage des besoins de chauffage  par énergie en fonction du CEREN 2010 
-//		public final static BigDecimal CalageConsoChauffElec 	= new BigDecimal("1.0508951");
-//		public final static BigDecimal CalageConsoChauffGaz 	= new BigDecimal("1.0016967");
-//		public final static BigDecimal CalageConsoChauffFioul 	= new BigDecimal("0.9932186");
-//		public final static BigDecimal CalageConsoChauffUrbain 	= new BigDecimal("0.9633484");
-//		public final static BigDecimal CalageConsoChauffAutres 	= new BigDecimal("0.9633484");
 
+	// parametres de recalage des consommations d'electricite specifique hors clim
+	// il s'agit ici de facteurs multiplicatifs des consommations d'electricite specifique hors clim
+	// METTRE 1 pour enlever le recalage
+	// NE PAS MODIFIER, utile pour une ancienne version du modele, laisser a 1
+		
 	// param recalage conso Elec specifique hors clim AME
 	//	public final static BigDecimal CalageConsoElecspe 	= new BigDecimal("1.28");
 		public final static BigDecimal CalageConsoElecspe 	= new BigDecimal("1");
 		
-	// param recalage conso Elec hors chauffage et elec spe
+	// parametres de recalage des consommations d'electricite hors chauffage et elec spe
+	// il s'agit ici de facteurs multiplicatifs des consommations d'electricite hors chauffage et elec spe
+	// METTRE 1 pour enlever le recalage
+	// NE PAS MODIFIER, utile pour une ancienne version du modele, laisser a 1
+				
 	//	public final static BigDecimal CalageConsoElecAutres	= new BigDecimal("1.0245238");
 		public final static BigDecimal CalageConsoElecAutres	= new BigDecimal("1");
-//		 
+
+	// parametres de recalage des consommations de chauffage par energie
+	// il s'agit ici de facteurs multiplicatifs des consommations de chauffage par energie
+	// METTRE 1 pour enlever le recalage
+	// NE PAS MODIFIER, utile pour une ancienne version du modele, laisser a 1
+			
 	// Décommenter pour enlever le recalage 
     //public final static boolean noCalage = false;
 //		public final static  BigDecimal  CalageBranche01 = BigDecimal.ONE;
--- model/src/main/java/com/ed/cgdd/derby/model/politiques.java
+++ model/src/main/java/com/ed/cgdd/derby/model/politiques.java
@@ -4,52 +4,62 @@ import java.util.HashMap;
 
 public class  politiques {
 	
-	// Activation politiques publiques
-	// individualisation des frais de chauffage
+	// Script d'activation de certaines politiques publiques 
+	// mettre true pour activer puis modifier les parametres pour la mesure
+	
+	//1) individualisation des frais de chauffage
 	public final static boolean checkIFC=true;
 	//public final static BigDecimal GainBU_2017 = new BigDecimal("0.993625");
 	//public final static BigDecimal GainBU_2018 = new BigDecimal("0.98725");
 	//public final static BigDecimal GainBU_2019 = new BigDecimal("0.980875");
+	
+	// Indiquer ici le gain annuel sur le besoin de chauffage en % sur 2017 2018 et 2019 (periode d'equipement du parc)  
 	public final static BigDecimal GainBU_IFC_annuel = new BigDecimal("-0.006375");
 
-	// RT existant 2018
+	//2) RT existant 2018 (hausse de la performance minimale des gestes et des systemes de chauffage)
 	public final static boolean checkRTex=true;
 
+	// Hausse du gain (en point de pourcentage) des gestes sur le bati
 	public final static BigDecimal GainSupRTex = new BigDecimal("0.06");
+	// Hausse du cout (en pourcentage) des gestes sur le bati
 	public final static BigDecimal CoutSupRTex = new BigDecimal("0.09");
 	
+	// Hausse du rendement (en pourcentage) des systemes de chauffage autres que electrique joule
 	public final static BigDecimal GainRdtSupRTex = new BigDecimal("0.05");
+	// Hausse du cout (en pourcentage) des systemes de chauffage autres que electrique joule
 	public final static BigDecimal CoutRdtSupRTex = new BigDecimal("0.075");
 	
+	// Hausse du rendement (en pourcentage) des systemes de chauffage  electrique joule
 	public final static BigDecimal GainRdtSupRTexElecJoule = new BigDecimal("0.05");
+	// Hausse du cout (en pourcentage) des systemes de chauffage electrique joule
 	public final static BigDecimal CoutRdtSupRTexElecJoule = new BigDecimal("0.075");
 	
-	// batiment exemplaire
-	public final static boolean  checkBatex =true;
+	// 3) batiment exemplaire de l'Etat (amelioration de la performance des batiments neufs de l'Etat à partir de 2017)
+	public final static boolean  checkBatex = true;
+	// facteur multiplicatif des besoins unitaires des usages RT des batiments entrants de l'Etat 
 	public final static BigDecimal modifBUBatEx = new BigDecimal("0.8325");
 
-	//travaux embarques
-	public final static boolean  checkTravEmb =true;
+	// 4) travaux embarques (obligation de renover de faire des travaux de renovation energetique 
+	// si on fait d'importants travaux sur un batiment type ravalement)
+	// Ne pas toucher, j'active cette mesure dans le fichier de parametre maintenant en la modelisant comme une obligation de renovation
+	// (vois parametres AME et AMS)
+	public final static boolean  checkTravEmb =false;
 	// taux de renovation tendanciel supplementaire
 	//public static float  txRenovTravEmb = 0.011f;
 	public static float  txRenovTravEmb = 0.0000001f;
 	
 	
-	//surcout RT 2012 electrique direct
-	public final static boolean  checkSurcoutRT2012 =true;
+	// 5) surcout RT 2012 electrique joule (surcout pour prendre en compte les couts d'isolation additionnels pour les batiments
+	// chauffes à l'electricite joule du fait de la RT 2012)
+	public final static boolean  checkSurcoutRT2012 = true;
 	// surcout en euros par m2 pour l'electrique joule
 	public final static BigDecimal surcoutRT = new BigDecimal("20");
 		
-	// CEE prix annuels
+	// 6) CEE modelisation avec des prix annee par annee pour la periode 2015 2016
+	// il y a differente valeurs commentes selon le scenario
 	public final static boolean  checkCEEannuels =true;
 	
-//	public final static BigDecimal pCEE2015 = new BigDecimal("0.020");
-//	public final static BigDecimal pCEE2016 = new BigDecimal("0.020");
-//	public final static BigDecimal pCEE2017 = new BigDecimal("0.020");
-//	public final static BigDecimal pCEE2018 = new BigDecimal("0.050");
-//	public final static BigDecimal pCEE2019 = new BigDecimal("0.050");
-//	public final static BigDecimal pCEE2020 = new BigDecimal("0.050");
-	
+	// prix 2015-2020 en euros par kwhcumac (valeur de la subvention accordee par kwhcumac)
 	public final static BigDecimal pCEE2015 = new BigDecimal("0.000");
 	public final static BigDecimal pCEE2016 = new BigDecimal("0.005");
 	public final static BigDecimal pCEE2017 = new BigDecimal("0.005");
@@ -57,56 +67,50 @@ public class  politiques {
 	public final static BigDecimal pCEE2019 = new BigDecimal("0.006");
 	public final static BigDecimal pCEE2020 = new BigDecimal("0.006");
 	
-	// prix apres 2020, mettre a zero pour arreter les CEE valeur AMS2
+	// prix initial et taux de croissance des prix apres 2020, mettre a zero pour arreter les CEE. ICI = valeur AMS
 	public final static BigDecimal pCEEsup2020 = new BigDecimal("0.006");
 	public final static BigDecimal tcamCEEsup2020 = new BigDecimal("0.012");
 	public final static BigDecimal tcamCEEsup2030 = new BigDecimal("0.07");
-	
-//	// prix apres 2020, mettre a zero pour arreter les CEE
+
+
+//// prix initial et taux de croissance des prix apres 2020, mettre a zero pour arreter les CEE. ICI = valeur AME
 //		public final static BigDecimal pCEEsup2020 = new BigDecimal("0");
 //		public final static BigDecimal tcamCEEsup2020 = new BigDecimal("0");
 //		public final static BigDecimal tcamCEEsup2030 = new BigDecimal("0");
-////valeur où on atteint 23 Twh en 2021 avec CINT subventionnés
-//	public final static BigDecimal pCEE2015 = new BigDecimal("0.070");
-//	public final static BigDecimal pCEE2016 = new BigDecimal("0.070");
-//	public final static BigDecimal pCEE2017 = new BigDecimal("0.100");
-//	public final static BigDecimal pCEE2018 = new BigDecimal("0.200");
-//	public final static BigDecimal pCEE2019 = new BigDecimal("0.300");
-//	public final static BigDecimal pCEE2020 = new BigDecimal("0.400");
-//	
-//	public final static BigDecimal pCEE2015 = new BigDecimal("0.053");
-//	public final static BigDecimal pCEE2016 = new BigDecimal("0.090");
-//	public final static BigDecimal pCEE2017 = new BigDecimal("0.100");
-//	public final static BigDecimal pCEE2018 = new BigDecimal("0.110");
-//	public final static BigDecimal pCEE2019 = new BigDecimal("0.140");
-//	public final static BigDecimal pCEE2020 = new BigDecimal("0.180");
+
+	// couts intangibles subventionnes (ancienne version du modele)
+	public final static boolean  checkCEECINT =true;
 	
-	// couts intangibles subventionnes 
-	public final static boolean  checkCEECINT =false;
-	
-	// Subvention aux economies d energie actualisees
+	// Subvention aux economies d energie actualisees (ancienne version du modele)
 	public final static boolean  checkSubEcoEner = false;
 		
 	// TODO faire une hasmap annee, pcee;
 	public HashMap<Integer, BigDecimal> pCEE = new HashMap<Integer, BigDecimal>();
 
-	// Adaptation CC TODO faire une Hasmap des tcam des besoins
+	
+	
+	// 7) Adaptation au CC (baisse du besoin unitaire de chauffage et hausse du besoin unitaire de climatisation du fait de la hausse 
+	// de la temperature exterieure
+	// (TODO faire une Hasmap des tcam des besoins)
 	public final static boolean  checkAdaptationCC = true;
-	// taux de croissance annuels moyens des besoin de chauffage par periode
-	public final static BigDecimal tcamBesoinChauff20152020 = new BigDecimal("-0.001068949531");
+	// taux de croissance annuels moyens des besoins de chauffage par periode
+	//	public final static BigDecimal tcamBesoinChauff20152020 = new BigDecimal("-0.001068949531");
+	public final static BigDecimal tcamBesoinChauff20152020 = new BigDecimal("0");
 	public final static BigDecimal tcamBesoinChauff20202025  = new BigDecimal("-0.001074693510");
 	public final static BigDecimal tcamBesoinChauff20252030  = new BigDecimal("-0.0010804995531");
 	public final static BigDecimal tcamBesoinChauff20302050  = new BigDecimal("-0.001914505186");
 
 	// taux de croissance annuels moyens des besoin de climatisation par periode
-	public final static BigDecimal tcamBesoinClim20152020 = new BigDecimal("0.007874988518");
+	//public final static BigDecimal tcamBesoinClim20152020 = new BigDecimal("0.007874988518");
+	public final static BigDecimal tcamBesoinClim20152020 = new BigDecimal("0");
 	public final static BigDecimal tcamBesoinClim20202025  = new BigDecimal("0.007576624052");
 	public final static BigDecimal tcamBesoinClim20252030  = new BigDecimal("0.007300045195");
 	public final static BigDecimal tcamBesoinClim20302050  = new BigDecimal("0.006304199098");
 
 	
-	// baisse du besoin en ECS (amelioration equipements, mitigeur)
+	// 8) MESURE AMS : baisse du besoin unitaire en ECS (amelioration des equipements, mitigeur)
 	public final static boolean checkBaisseBesoinECS =true;
+	// taux de croissance annuel moyen des besoin en ECS
 	public final static BigDecimal tcamBesoinECS20152050 = new BigDecimal("-0.0063552495"); 
 
 	
